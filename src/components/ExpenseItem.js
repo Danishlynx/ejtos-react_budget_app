9015a0e438c77bb8a47db028e5ed974bea92dc54
Danishlynx <danishlynx@gmail.com>
2024-01-02 07:03:27 -0500

    test

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -36,15 +36,50 @@ const ExpenseItem = (props) => {
         });
     };
 
-    return (
+        return (
         <tr>
             <td>{props.name}</td>
             <td>{currency}{props.cost}</td>
-            <td><button onClick={() => increaseAllocation(props.name)}>+</button></td>
-            <td><button onClick={() => decreaseAllocation(props.name)}>-</button></td>
-            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>
+                <button 
+                    onClick={() => increaseAllocation(props.name)} 
+                    style={{
+                        backgroundColor: '#4CAF50', // Green background
+                        color: 'white', // White text
+                        border: 'none',
+                        padding: '5px',
+                        borderRadius: '25px',
+                        cursor: 'pointer',
+                    }}
+                >
+                    +
+                </button>
+            </td>
+            <td>
+                <button 
+                    onClick={() => decreaseAllocation(props.name)} 
+                    style={{
+                        backgroundColor: '#f44336', // Red background
+                        color: 'white', // White text
+                        border: 'none',
+                        padding: '5px',
+                        borderRadius: '20px',
+                        cursor: 'pointer',
+                    }}
+                >
+                    -
+                </button>
+            </td>
+            <td>
+                <TiDelete 
+                    size='1.5em' 
+                    onClick={handleDeleteExpense}
+                    style={{ cursor: 'pointer' }}
+                />
+            </td>
         </tr>
     );
+
 };
 
 export default ExpenseItem;
